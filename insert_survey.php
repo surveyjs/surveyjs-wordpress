10c848efdeb8a1b8b83b72e65bbe2eadb0173691
dmitrykurmanov <kurmanov.work@gmail.com>
2018-01-24 14:17:17 +0300

    added "insert" button into "choose survey" dialog

--- insert_survey.php
+++ insert_survey.php
@@ -14,16 +14,25 @@ class WP_InsertSurveyHandler extends AJAX_Handler {
         //wp_send_json_success(array('test'=>'Works!'));
         $client = new WP_Service_Client("dc736a6f384d48f9b71a1dd94d9d5c24");
         ?>
-        <select>
-            <option>- Choose survey to insert -</option>
-            <?php
-            foreach ($client->getSurveys() as $surveyDefinition) {
-                ?>
-                <option value="<?php echo $surveyDefinition->Id ?>"><?php echo $surveyDefinition->Name ?></option>
+            <script>
+                function insert_survey_shorttext() {
+                    var select = document.querySelector("select[name='survey-name']");
+                    var text = '[Survey id="' + select.value + '"]';
+                    window.parent.send_to_editor(text);
+                    window.parent.tb_remove();
+                }
+            </script>
+            <select name="survey-name">
+                <option>- Choose survey to insert -</option>
                 <?php
-            }
-            ?>
+                foreach ($client->getSurveys() as $surveyDefinition) {
+                    ?>
+                    <option value="<?php echo $surveyDefinition->Id ?>"><?php echo $surveyDefinition->Name ?></option>
+                    <?php
+                }
+                ?>
             </select>
+            <button onclick="insert_survey_shorttext()">Insert</button>
         <?php
     }
 }
