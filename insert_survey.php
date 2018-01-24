fbdddfbe03115c78e29f5211ab49f0eba368bb11
tsv2013 <tsv2013@users.noreply.github.com>
2018-01-24 11:05:03 +0300

    Added "choose survey" control to the "Insert survey" popup window

--- insert_survey.php
+++ insert_survey.php
@@ -1,19 +1,29 @@
 <?php
 
 include("ajax_handler.php");
+include("service_client.php");
 
 class WP_InsertSurveyHandler extends AJAX_Handler {
     
     function __construct() {
         parent::__construct("WP_SJS_InsertSurvey");
+        
     }
         
     function callback() {
         //wp_send_json_success(array('test'=>'Works!'));
+        $client = new WP_Service_Client("dc736a6f384d48f9b71a1dd94d9d5c24");
         ?>
         <select>
             <option>- Choose survey to insert -</option>
-        </select>
+            <?php
+            foreach ($client->getSurveys() as $surveyDefinition) {
+                ?>
+                <option value="<?php echo $surveyDefinition->Id ?>"><?php echo $surveyDefinition->Name ?></option>
+                <?php
+            }
+            ?>
+            </select>
         <?php
     }
 }
---
+++ service_client.php
@@ -0,0 +1,22 @@
+<?php
+
+class WP_Service_Client {
+    private $accessKey;
+
+    function __construct($accessKey) {
+        $this->accessKey = $accessKey;
+    }
+
+    public function getSurveys() {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, "https://surveyjs.io/api/MySurveys/getActive?ownerId=null&accessKey=".$this->accessKey);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
+    }
+
+}
+
+?>
