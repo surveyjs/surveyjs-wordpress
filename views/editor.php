584195d441d1c01c225c3e1cc3d3ba91818bd2c0
tsv2013 <tsv2013@users.noreply.github.com>
2018-01-25 11:04:48 +0300

    Fixed editor styles

--- views/editor.php
+++ views/editor.php
@@ -8,6 +8,16 @@ class WP_SJS_Editor {
 
     public static function render() {
         ?>
+            <style>
+                #sjs-editor-container .svd_container .card {
+                    max-width: none;
+                }
+                #sjs-editor-container .svd_container select {
+                    height: 34px;
+                    line-height: 20px;
+                    padding: 6px 12px;
+                }
+            </style>
             <div class="wrap">
                 <h2><?php _e( 'Survey Editor', 'sjs' ); ?></h2>
                 <div id="sjs-editor-container"></div>
