d32410e4c45cfb2c7fff0fb525d552cfd66237c9
tsv2013 <tsv2013@users.noreply.github.com>
2018-01-26 19:55:58 +0300

    Working OAuth2 client

--- surveyjs-oauth2-client.php
+++ surveyjs-oauth2-client.php
@@ -1,9 +1,9 @@
 <?php
 define('OAUTH2_CLIENT_ID', '1234561');
 define('OAUTH2_CLIENT_SECRET', 'abcdef');
-$authorizeURL = 'https://localhost:44388/OAuth/Authorize';
-$tokenURL = 'https://localhost:44388/OAuth/Token';
-$apiURLBase = 'https://localhost:44388/api/Resource/Profile';
+$authorizeURL = 'https://surveyjsio-stage.azurewebsites.net/OAuth/Authorize';
+$tokenURL = 'https://surveyjsio-stage.azurewebsites.net/OAuth/Token';
+$apiURLBase = 'https://surveyjsio-stage.azurewebsites.net/api/Resource/Profile';
 session_start();
 // Start the login process by sending the user to Github's authorization page
 if(get('action') == 'login') {
@@ -31,7 +31,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['code']) {
     die();
   }
   // Exchange the auth code for a token
-  $token = apiRequest(/*$tokenURL*/'http://localhost:8080', array(
+  $token = apiRequest($tokenURL, array(
     'client_id' => OAUTH2_CLIENT_ID,
     'client_secret' => OAUTH2_CLIENT_SECRET,
     'grant_type' => 'authorization_code',
@@ -42,30 +42,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['code']) {
   ));
   $_SESSION['access_token'] = $token->access_token;
 
-/*
-  $ch1 = curl_init($tokenURL);
-  curl_setopt($ch1, CURLOPT_POST, 1);
-  curl_setopt($ch1,CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch1, CURLOPT_POSTFIELDS, 
-    'client_id='.OAUTH2_CLIENT_ID.'&'.
-    'client_secret='.OAUTH2_CLIENT_SECRET.'&'.
-    'grant_type='.'authorization_code'.'&'.
-    'redirect_uri='.'http://localhost:8080/surveyjs-oauth2-client.php'.'&'.
-    'state='.$_SESSION['state'].'&'.
-    'code='.$_POST['code']
-  );
-  curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-  curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-  $server_output = curl_exec($ch1);
-  curl_close($ch1);
-*/
-
-  header('Location: ' . $_SERVER['PHP_SELF'].'#good'.$token/*->access_token*/);
+  header('Location: ' . $_SERVER['PHP_SELF'].'#good='.$token->access_token);
 }
 if(session('access_token')) {
-  $user = apiRequest($apiURLBase);
+  $profile = apiRequest($apiURLBase, array('1' => 2));
   echo '<h3>Logged In</h3>';
-  echo '<h4>' . $user->name . '</h4>';
+  echo '<h4>' . $profile->PublicName . '</h4>';
   echo '<pre>';
   print_r($user);
   echo '</pre>';
@@ -75,28 +57,16 @@ if(session('access_token')) {
 }
 function apiRequest($url, $post=FALSE, $headers=array()) {
   $ch = curl_init($url);
-  //curl_setopt ($ch, CURLOPT_PORT, 44388); 
-  //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   if($post)
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-  //$headers[] = 'User-Agent: php-test';
   $headers[] = 'Accept: application/json';
   if(session('access_token'))
     $headers[] = 'Authorization: Bearer ' . session('access_token');
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   $response = curl_exec($ch);
   $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-  if ( $httpCode != 200 ) {
-    $response = $httpCode;
-  } else {
-    $response = 2;
-  }
-  curl_close($ch);
-  //return json_decode($response);
-  return $response;
+  return json_decode($response);
 }
 function get($key, $default=NULL) {
   return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
