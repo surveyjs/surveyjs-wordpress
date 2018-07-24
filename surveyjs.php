62c1cd465b8fa402dc3846b62742814e68c253ca
tsv2013 <tsv2013@users.noreply.github.com>
2018-07-24 09:48:01 +0300

    Updated surveyjs scripts and bumped version

--- surveyjs.php
+++ surveyjs.php
@@ -3,7 +3,7 @@
 Plugin Name: SurveyJS
 Plugin URI: https://wordpress.org/plugins/surveyjs
 Description: Easy to use, drag & drop Survey Builder with myriad options.
-Version: 1.0.26
+Version: 1.0.33
 Author: Devsoft Baltic OÜ
 Author URI: http://devsoftbaltic.com/
 */
