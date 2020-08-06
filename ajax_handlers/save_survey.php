5b9060cb451561e041dd868cc8edffe8f7eb453b
selestos <adrian.fedo3enko@gmail.com>
2020-08-06 20:21:13 +0300

    HTML tags filtering | https://github.com/surveyjs/surveyjs-wordpress/issues/7

--- ajax_handlers/save_survey.php
+++ ajax_handlers/save_survey.php
@@ -3,26 +3,26 @@
 include_once("ajax_handler.php");
 
 class SurveyJS_SaveSurvey extends SurveyJS_AJAX_Handler {
-    
+
     function __construct() {
-        parent::__construct("SurveyJS_SaveSurvey", false);  
+        parent::__construct("SurveyJS_SaveSurvey", false);
     }
-        
+
     function callback() {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = sanitize_key($_POST['Id']);
-            $json = sanitize_text_field($_POST['Json']);
+            $json = current_user_can( 'unfiltered_html' ) ? $_POST['Json'] : wp_kses_post( $_POST['Json'] );
 
             if(!!$json) {
                 global $wpdb;
                 $table_name = $wpdb->prefix . 'sjs_my_surveys';
 
-                $result = $wpdb->update( 
-                    $table_name, 
-                    array( 
+                $result = $wpdb->update(
+                    $table_name,
+                    array(
                         'json' => $json
                     ),
-                    array( 
+                    array(
                         'id' => intval($id)
                     )
                 );
