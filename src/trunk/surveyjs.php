d0f6b4a429706b450c1dd949feb7d00e41e390d6
dmitrykurmanov <kurmanov.work@gmail.com>
2024-03-27 12:38:54 +0400

    updated surveyjs libs to 1.9.136

--- src/trunk/surveyjs.php
+++ src/trunk/surveyjs.php
@@ -3,7 +3,7 @@
 Plugin Name: SurveyJS
 Plugin URI: https://wordpress.org/plugins/surveyjs
 Description: Easy to use, drag & drop Survey Builder with myriad options.
-Version: 1.9.120
+Version: 1.9.136
 Author: Devsoft Baltic OÜ
 Author URI: http://devsoftbaltic.com/
 */
