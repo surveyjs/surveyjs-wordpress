5b76a0a1cf6142e20507c4f6c508464f71436eaa
dmitrykurmanov <kurmanov.work@gmail.com>
2024-08-06 17:59:02 +0400

    work for the https://github.com/surveyjs/surveyjs-wordpress/issues/51

--- src/trunk/ajax_handlers/save_result.php
+++ src/trunk/ajax_handlers/save_result.php
@@ -9,19 +9,25 @@ class SurveyJS_SaveResult extends SurveyJS_AJAX_Handler {
     }
         
     function callback() {
+        $SurveyId = intval(sanitize_key($_POST['SurveyId']));
+        $Json =  sanitize_text_field($_POST['Json']);
+
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            do_action('surveyjs_save_result');
-
-            global $wpdb;
-            $table_name = $wpdb->prefix . 'sjs_results';
-
-            $wpdb->insert( 
-                $table_name, 
-                array( 
-                 'surveyId' => intval(sanitize_key($_POST['SurveyId'])),
-                 'json' => sanitize_text_field($_POST['Json'])
-                ) 
-            );
+            if (function_exists('surveyjs_save_result'))
+            {
+                do_action('surveyjs_save_result', $SurveyId, $Json);
+            } else {
+                global $wpdb;
+                $table_name = $wpdb->prefix . 'sjs_results';
+    
+                $wpdb->insert( 
+                    $table_name, 
+                    array( 
+                     'surveyId' => $SurveyId,
+                     'json' => $Json
+                    ) 
+                );
+            }
         }
     }
 }
